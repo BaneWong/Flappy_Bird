6cb47c65d513cde965f662f96f330484252efa1a
xiangz <xz820@nyu.edu>
2014-02-25 20:40:12 -0500

    first commit

---
+++ FlappyBird/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  FlappyBird
+//
+
+//  Copyright (c) 2014年 xiang zhang. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ FlappyBird/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlappyBird
+//
+//  Created by xiang zhang on 14-2-19.
+//  Copyright (c) 2014年 xiang zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
